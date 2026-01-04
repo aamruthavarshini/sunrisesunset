13fd4174707223fbf344b55eb143d7559bf670b4
aamruthavarshini <amruthavarshini601@gmail.com>
2026-01-04 18:12:48 +0530

    Initial commit - Spring Boot sunrise sunset project

---
+++ src/main/java/com/example/sunrisesunset/SunrisesunsetApplication.java
@@ -0,0 +1,13 @@
+package com.example.sunrisesunset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunrisesunsetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunrisesunsetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sunrisesunset/SunrisesunsetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sunrisesunset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunrisesunsetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
